8f2d821b1ceba826f6daa1651f78bc38fc754e5b
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-11-18 08:56:13 +0200

    A

--- src/contentful/contentful.ts
+++ src/contentful/contentful.ts
@@ -1,6 +1,6 @@
 import { createClient, ContentfulClientApi } from "contentful";
 
 export const contentful: ContentfulClientApi<undefined> = createClient({
-    space: process.env.SPACE_ID || '' as string,
-    accessToken: process.env.CONTENTFUL_ACCESS || '' as string,
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '' as string,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || '' as string,
 });
